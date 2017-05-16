c7aa66a0aae0b506bd16249624a200afd250c04b
michalper <michal.per@encja.com>
2017-05-16 10:27:11 +0200

    added error response

---
+++ lib/SalesForceClient/Model/ErrorResponseModel.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace SalesForceClient\Model;
+
+/**
+ * Class ErrorResponseModel
+ * @package SalesForceClient\Model
+ */
+class ErrorResponseModel
+{
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * @var string
+     */
+    private $errorCode;
+
+    /**
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     * @return ErrorResponseModel
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getErrorCode()
+    {
+        return $this->errorCode;
+    }
+
+    /**
+     * @param string $errorCode
+     * @return ErrorResponseModel
+     */
+    public function setErrorCode($errorCode)
+    {
+        $this->errorCode = $errorCode;
+        return $this;
+    }
+
+
+}
